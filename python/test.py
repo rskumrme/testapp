a0c4f3b818d217341d00f7727f7a4e49b7b4916c
rskumrme <147756932+rskumrme@users.noreply.github.com>
2025-03-07 20:56:36 +0530

    Add files via upload

---
+++ python/test.py
@@ -0,0 +1,25 @@
+import sys
+
+# Create a sample collection
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy:  Create a new collection
+active_users = {}
+print (sys.getsizeof(active_users))
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
+
+print (active_users);
+print (sys.getsizeof(active_users))
+print (len(active_users))
+
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for f in sorted(set(basket)):
+    print(f)
