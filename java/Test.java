4686bc8d6006ae1a347ff2ae1ae198bfe18d046f
rskumrme <147756932+rskumrme@users.noreply.github.com>
2025-03-08 20:28:41 +0530

    Update Test.java

--- java/Test.java
+++ java/Test.java
@@ -1,5 +1,9 @@
 public class Test {
   public static void main(String[] args) {
+    String txt = "Hello " + "World";
+    System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+    System.out.println(txt.toLowerCase());   // Outputs "hello world"
+
     String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     System.out.println("The length of the txt string is: " + txt.length());
   }
